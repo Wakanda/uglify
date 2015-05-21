babd292c67061672dde3d4770feaa8ecc74c3a5a
François Marchal <francois.marchal@4d.com>
2015-05-21 16:24:18 +0200

    using try catch for better error handling

--- index.js
+++ index.js
@@ -21,9 +21,9 @@
 
 
 function uglify(original_code){
-    var jsp = require("./lib/parse-js");
-    var pro = require("./lib/process");
-    var consolidator = require("./lib/consolidator");
+	var jsp = require("./lib/parse-js");
+	var pro = require("./lib/process");
+	var consolidator = require("./lib/consolidator");
 	var ast = jsp.parse(original_code); // parse code and get the initial AST
 	ast = pro.ast_mangle(ast); // get a new AST with mangled names
 	ast = pro.ast_squeeze(ast); // get an AST with compression optimizations
@@ -33,50 +33,76 @@ function uglify(original_code){
 
 //point d'entree unique de l'extension
 exports.handleMessage = function handleMessage(message) {
-    var 
+	var 
 		actionName;
 
-    if (message.action == 'uglify') {
-        var original_code = studio.currentEditor.getContent();
-        var smaller_code = uglify(original_code);
-        studio.currentEditor.selectFromStartOfText(0, 1000000);
-        studio.currentEditor.insertText(smaller_code);
-        var ratio = 100 * (smaller_code.length / original_code.length);
-        studio.alert('Original size: ' + original_code.length + '\n'
-        			   + 'Uglified size: ' + smaller_code.length + '\n'
-        			   + 'Compression ratio: ' + ratio.toFixed(2) + '%');
-    }
-    else if (message.action == 'uglifyFile') {
-        var selectedFiles = studio.currentSolution.getSelectedItems();
-        var original_length = 0;
-        var smaller_length = 0;
-        for (var i = 0; i < selectedFiles.length; i++) {
-            var jsFile = selectedFiles[i];
-            var filePath = jsFile.path;
-            var uglifiedPath = filePath.substr(0, filePath.length - jsFile.extension.length - 1);
+	if (message.action == 'uglify') {
+		var original_code = studio.currentEditor.getContent();
+		var smaller_code;
+		var showResults = true;
+		
+		try {
+		 	smaller_code = uglify(original_code);
+		}
+		
+		catch (err ) {
+			studio.alert( err.message );
+			showResults = false;
+		}
+		
+		if ( showResults ) {
+			studio.currentEditor.selectFromStartOfText(0, 1000000);
+	   		studio.currentEditor.insertText(smaller_code);
+	   		var ratio = 100 * (smaller_code.length / original_code.length);
+	   		studio.alert('Original size: ' + original_code.length + '\n'
+	   		 			   + 'Uglified size: ' + smaller_code.length + '\n'
+	   		 			   + 'Compression ratio: ' + ratio.toFixed(2) + '%');
+   		 }
+	}
+	else if (message.action == 'uglifyFile') {
+		var selectedFiles = studio.currentSolution.getSelectedItems();
+		var original_length = 0;
+		var smaller_length = 0;
+		for (var i = 0; i < selectedFiles.length; i++) {
+			var jsFile = selectedFiles[i];
+			var filePath = jsFile.path;
+			var uglifiedPath = filePath.substr(0, filePath.length - jsFile.extension.length - 1);
+			var saveFile = true;
 
-            uglifiedPath += '-ugl.';
-            uglifiedPath += jsFile.extension;
-            var jsUglifiedFile = File(uglifiedPath);
+			uglifiedPath += '-ugl.';
+			uglifiedPath += jsFile.extension;
+			var jsUglifiedFile = File(uglifiedPath);
 
-            if (!jsUglifiedFile.exists)
-                jsUglifiedFile.create();
+			if (!jsUglifiedFile.exists)
+				jsUglifiedFile.create();
 
-            var original_code = jsFile.toString();
-            var smaller_code = uglify(original_code);
+			var original_code = jsFile.toString();
+			
+			var smaller_code;
+			
+			try {
+				smaller_code = uglify(original_code);
+			}
+			catch (err ) {
+				studio.alert( 'Error on file "' + filePath + '": ' + err.message );
+				saveFile = false;
+			}
+			
+			if ( saveFile ) {
+				original_length += original_code.length;
+				smaller_length += smaller_code.length;
 
-            original_length += original_code.length;
-            smaller_length += smaller_code.length;
+				var stream = new TextStream(jsUglifiedFile, "Overwrite");
+				stream.write(smaller_code);
+				stream.close();
+			}
+		}
 
-            var stream = new TextStream(jsUglifiedFile, "Overwrite");
-            stream.write(smaller_code);
-            stream.close();
-        }
-
-        var ratio = 100 * (smaller_length / original_length);
-        studio.alert('Original size: ' + original_length + '\n'
-        			  + 'Uglified size: ' + smaller_length + '\n'
-        			  + 'Compression ratio: ' + ratio.toFixed(2) + '%');
-
-    }
+		if ( original_length > 0 ) {
+			var ratio = 100 * (smaller_length / original_length);
+			studio.alert('Original size: ' + original_length + '\n'
+						  + 'Uglified size: ' + smaller_length + '\n'
+						  + 'Compression ratio: ' + ratio.toFixed(2) + '%');
+		}
+	}
 }
